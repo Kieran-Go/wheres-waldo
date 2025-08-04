ef81a01a73f6c8d7daa22401d4822764c6239aba
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2025-08-04 12:00:00 +0930

    Add mock data and render scenes on Home page
    - Created a mock database with scenes and character data
    - Rendered scenes dynamically from mock database onto the Home page
    - Styled scene cards with grid layout and responsive design

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import '../css/Home.css';
 import HomeHeader from "./HomeHeader";
+import db from "../mockData";
 
 export default function Home() {
   // Change document title
@@ -7,9 +10,37 @@ export default function Home() {
     document.title = "Where's Waldo?";
   }, []);
 
+  // Retrieve scene data from the db
+  const scenes = db;
+
   return (
     <>
       <HomeHeader />
+
+      {/* Render all scenes */}
+      <div className="scenes-container">
+        {scenes.map((scene) => {
+          return (
+            <Link className="scene-link" to={`/scene/${scene.id}`} key={scene.id}>
+              <div className="scene-card">
+                <img src={scene.imageUrl} alt={`Thumbnail for ${scene.name}`} className="scene-img"/>
+                <div className="scene-info">
+                  <h3>{scene.name}</h3>
+                  <div className="characters-container">
+                    {scene.characters.map((character) => {
+                      return(
+                        <div className="characters-container">
+                          <img key={character.id} className="character-img" src={character.imageUrl} alt={character.name}/>
+                        </div>
+                      )
+                    })}
+                  </div>
+                </div>
+              </div>
+            </Link>
+          )
+        })}
+      </div>
     </>
   )
 }
