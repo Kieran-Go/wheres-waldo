225b5455b34907156fb4dc16bcd23bb736555534
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2025-08-15 08:21:02 +0930

    Add delayed loading message for server cold starts
    - Updated Loading component to display a message after 5 seconds of
      loading to inform users that Render servers may be waking up
    - Helps set expectations for longer load times caused by server cold
      starts

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,13 +1,32 @@
 import '../css/Loading.css';
 import HomeHeader from './HomeHeader';
+import { useEffect, useState } from 'react';
 
 export default function Loading({ message, showHeader = true }) {
+  const [loadTime, setLoadTime] = useState(0);
+  const [wakingServerMessage, setWakingServerMessage] = useState(false);
+
+  // If loading takes more than 5 seconds, assume the Render servers are waking up and display
+  // a message asking the user to wait for up to one minute
+  useEffect(() => {
+    if(!wakingServerMessage) {
+      const interval = setInterval(() => {
+        setLoadTime(prev => prev + 1);
+        if (loadTime >= 5) {
+          setWakingServerMessage("Servers are starting up — this can take up to a minute. Thanks for your patience!");
+        }
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  },[loadTime]);
+
   return (
     <>
       {showHeader && (<HomeHeader />)}
       <div className="loading__container">
         <div className="loading__spinner"></div>
         {message && <p className="loading__message">{message}</p>}
+        {wakingServerMessage && <p className="loading__message">{wakingServerMessage}</p>}
       </div>
     </>
   );
